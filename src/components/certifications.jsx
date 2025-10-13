0542bea6db57e5a871e6ddfcdc8da9637247423e
purvajamandlik <purvajamandlik@gmail.com>
2025-10-13 15:36:56 +0530

    initial commit

--- src/components/certifications.jsx
+++ src/components/certifications.jsx
@@ -1,23 +1,71 @@
-
+import React from 'react';
 
 const Certification = () => {
   return (
-<div className="info-box">
-    <h1>Certifications</h1>
-    <div className="cert-list">
-      <a href="#">Red Hat RH104 - Linux Fundamentals 9.1</a>
-      <a href="#">Red Hat RH124 - System Administration I 9.3</a>
-      <a href="#">Red Hat RH134 - System Administration II 9.3</a>
-      <a href="https://www.linkedin.com/posts/purvaja-mandlik_activity-7235927685972000768--XhP?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEY5uc0B1zH9fh_jWo-XTSoVtC-HA1WZX08">Oracle OCI 2024 - Data Foundations Associate</a>
-      <a href="https://www.linkedin.com/posts/purvaja-mandlik_activity-7236368085237022720-GEvz?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEY5uc0B1zH9fh_jWo-XTSoVtC-HA1WZX08">Oracle OCI 2024 - AI Foundations Associate</a>
-      <a href="https://www.coursera.org/account/accomplishments/specialization/20IPGLUNLL58">Coursera - Software Engineering Specialization</a>
-      <a href="https://www.coursera.org/account/accomplishments/specialization/I7C6GY8GWG8G">Coursera - Blockchain Specialization</a>
-      <a href="https://www.linkedin.com/posts/purvaja-mandlik_django-webdevelopment-coursera-activity-7315429475406503936-JV43?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEY5uc0B1zH9fh_jWo-XTSoVtC-HA1WZX08">Coursera - Django Web Framework</a>
-      <a href="https://www.linkedin.com/posts/purvaja-mandlik_django-fullstackdevelopment-coursera-activity-7318551031091544065-x63d?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEY5uc0B1zH9fh_jWo-XTSoVtC-HA1WZX08">Coursera - Full-Stack Development with Django</a>
-      <a href="https://www.linkedin.com/posts/purvaja-mandlik_cybersecurity-learning-coursera-activity-7325833530792394753-EP00?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEY5uc0B1zH9fh_jWo-XTSoVtC-HA1WZX08">Coursera - Foundation of Cybersecurity</a>
+    <div className="info-box">
+      <h1>Certifications</h1>
+      <div className="cert-list">
+        <a href="#" onClick={(e) => e.preventDefault()}>
+          Red Hat RH104 - Linux Fundamentals 9.1
+        </a>
+        <a href="#" onClick={(e) => e.preventDefault()}>
+          Red Hat RH124 - System Administration I 9.3
+        </a>
+        <a href="#" onClick={(e) => e.preventDefault()}>
+          Red Hat RH134 - System Administration II 9.3
+        </a>
+        <a
+          href="https://www.linkedin.com/posts/purvaja-mandlik_activity-7235927685972000768--XhP?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEY5uc0B1zH9fh_jWo-XTSoVtC-HA1WZX08"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Oracle OCI 2024 - Data Foundations Associate
+        </a>
+        <a
+          href="https://www.linkedin.com/posts/purvaja-mandlik_activity-7236368085237022720-GEvz?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEY5uc0B1zH9fh_jWo-XTSoVtC-HA1WZX08"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Oracle OCI 2024 - AI Foundations Associate
+        </a>
+        <a
+          href="https://www.coursera.org/account/accomplishments/specialization/20IPGLUNLL58"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coursera - Software Engineering Specialization
+        </a>
+        <a
+          href="https://www.coursera.org/account/accomplishments/specialization/I7C6GY8GWG8G"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coursera - Blockchain Specialization
+        </a>
+        <a
+          href="https://www.linkedin.com/posts/purvaja-mandlik_django-webdevelopment-coursera-activity-7315429475406503936-JV43?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEY5uc0B1zH9fh_jWo-XTSoVtC-HA1WZX08"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coursera - Django Web Framework
+        </a>
+        <a
+          href="https://www.linkedin.com/posts/purvaja-mandlik_django-fullstackdevelopment-coursera-activity-7318551031091544065-x63d?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEY5uc0B1zH9fh_jWo-XTSoVtC-HA1WZX08"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coursera - Full-Stack Development with Django
+        </a>
+        <a
+          href="https://www.linkedin.com/posts/purvaja-mandlik_cybersecurity-learning-coursera-activity-7325833530792394753-EP00?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEY5uc0B1zH9fh_jWo-XTSoVtC-HA1WZX08"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coursera - Foundation of Cybersecurity
+        </a>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Certification
+export default Certification;
